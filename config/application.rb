882f4a9764c94e0f6c3666a2bcd68971a7c51160
Frankyston Lins <frankyston@gmail.com>
2020-12-03 17:06:12 -0300

    fix dotenv

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Dotenv::Railtie.load
+Dotenv::Railtie.load if Rails.env.development?
 
 module Testjob
   class Application < Rails::Application
